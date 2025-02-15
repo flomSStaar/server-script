dfeae88fa5d1fd32a15b8ca248e6aa9fea4b0159
Florent MARQUES <f.marques@overscan.fr>
2025-02-15 22:33:48 +0100

    :lipstick: improve script output

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -12,7 +12,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+apt-get update > /dev/null
 
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
 
