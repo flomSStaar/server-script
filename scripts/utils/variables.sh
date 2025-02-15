42d59f7cb612734f0d1303042d7e26433aaf7171
Florent MARQUES <f.marques@overscan.fr>
2025-02-15 21:38:18 +0100

    :bug: fix DIR variable

--- scripts/utils/variables.sh
+++ scripts/utils/variables.sh
@@ -2,10 +2,6 @@ ROOT_SCRIPT_DIR="/root/scripts"
 SECRETS_DIR="/root/secrets"
 SERVICES_DIR="/home/services"
 
-# Get the directory of the running script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 export ROOT_SCRIPT_DIR
 export SECRETS_DIR
 export SERVICES_DIR
-export DIR
