57d66b17e1a25dbfa8432337fbeb5eea2791e12f
Florent MARQUES <f.marques@overscan.fr>
2025-02-17 16:32:53 +0100

    :sparkles: add hostname replacement in common module

--- scripts/common/setup.sh
+++ scripts/common/setup.sh
@@ -9,3 +9,14 @@ info "Configuring .bashrc for root user"
 cat "${DIR}/common/bashrc" > /root/.bashrc
 
 success "Configured .bashrc for root user"
+
+info "Configuring hostname"
+
+OLD_HOSTNAME=$(hostname)
+
+info "Replacing hostname \"${OLD_HOSTNAME}\" with \"${SERVER_HOSTNAME}\""
+
+hostnamectl set-hostname "${SERVER_HOSTNAME}"
+sed -i "s/${OLD_HOSTNAME}/${SERVER_HOSTNAME}/g" /etc/hosts
+
+success "Configured hostname: ${SERVER_HOSTNAME}"
